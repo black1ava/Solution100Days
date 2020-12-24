89d742190f4014b38f18f5fbca0a0bcafe53ff70
Black1ava <roeunboratharath@gmail.com>
2020-12-25 01:35:12 +0700

    Add Solution 1 in C
    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
    
    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.

--- C/Solution_Day_1.c
+++ C/Solution_Day_1.c
@@ -1,5 +1,32 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+bool addUp(int arr[], int length, int k){
+  for(int i = 0; i < length; i++){
+    for(int j = i + 1; j < length; j++){
+      if(arr[i] + arr[j] == k){
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 int main(){
-  
+  int n, k;
+  printf("How many numbers in the list: ");
+  scanf("%d", &n);
+  int list[n];
+  printf("Input all numbers in one line seperate by space: ");
+  for(int i = 0; i < n; i++){
+    scanf("%d", &list[i]);
+  }
+  printf("Input a k number: ");
+  scanf("%d", &k);
+  if(addUp(list, n, k)){
+    printf("True");
+  }else{
+    printf("False");
+  }
+  return 0;
 }
