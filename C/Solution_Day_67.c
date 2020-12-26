8aeb6af559dba226beebc7434073ab047be41593
Black1ava <roeunboratharath@gmail.com>
2020-12-26 18:03:56 +0700

    Update Solution
    This question was asked by ContextLogic.
    
    Implement division of two positive integers without using the division, multiplication, or modulus operators. Return the quotient as an integer, ignoring the remainder.

--- C/Solution_Day_67.c
+++ C/Solution_Day_67.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int divide(int a, int b, int *result){
   if(a < b){
@@ -13,7 +14,16 @@ int main(){
   int a, b, result = 0;
   printf("Input 2 positive numbers seperate by space: ");
   scanf("%d%d", &a, &b);
-  divide(a, b, &result);
-  printf("%d ÷ %d = %d", a, b, result);
+  if(b == 0){
+    printf("Error");
+  }else{
+    divide(abs(a), abs(b), &result);
+    if(a < 0 || b < 0){
+      printf("%d ÷ %d = %d", a, b, -result);
+    }else{
+      printf("%d ÷ %d = %d", a, b, result);
+    }
+  }
+  
   return 0;
 }
