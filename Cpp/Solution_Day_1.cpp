709ed932c6e467c0c4fb95b52b2706cb8b8dece3
Black1ava <roeunboratharath@gmail.com>
2021-01-11 01:14:16 +0700

    Add Solution Day 1
    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
    
    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.

--- Cpp/Solution_Day_1.cpp
+++ Cpp/Solution_Day_1.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
 using namespace std;
 
+bool addUp(int arr[], int n, int k){
+  for(int i = 0; i < n; i++){
+    
+    for(int j = 0; j < n; j++){
+      if(arr[i] + arr[j] == k){
+        return true;
+      }
+    }
+    
+  }
+  
+  return false;
+}
+
 int main(){
-  int 
+  int n, k;
+  cout << "Amount of elements: ";
+  cin >> n;
+  
+  int arr[n];
+  cout << "Input all numbers in one line seperate by space : ";
+  
+  for(int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  
+  cout << "Input a number k : ";
+  cin >> k;
+  
+  if(addUp(arr, n, k)){
+    cout << "True";
+  }else{
+    cout << "False";
+  }
+  
+  return 0;
 }
