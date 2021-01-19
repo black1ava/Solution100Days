d2997ac574510c43f25d1634172dc6f88daf5a08
black1ava <73036266+black1ava@users.noreply.github.com>
2021-01-19 23:23:15 +0700

    Update

--- C/OS/Find Max/solution1.c
+++ C/OS/Find Max/solution1.c
@@ -1,17 +1,15 @@
 #include<stdio.h> //time complexity O(n)
 #include<limits.h>
 
-#define L 6
-
 int max = INT_MIN;
 
 int larger(int a, int b){
   return a > b ? a : b;
 }
 
-int maximum(int *arr){
+int maximum(int *arr, int length){
   
-  for(int i = 0; i < L; i++){
+  for(int i = 0; i < length; i++){
     if(*(arr + i) > max){
       
       max = *(arr + i);
@@ -22,14 +20,17 @@ int maximum(int *arr){
 }
 
 int main(){
-  int arr[L];
+  int n;
+  printf("Elements in an array : ");
+  scanf("%d", &n);
+  int arr[n];
   
-  printf("Input 6 integers in one line seperate by space : ");
+  printf("Input %d integers in one line seperate by space : ", n);
   
-  for(int i = 0; i < L; i++){
+  for(int i = 0; i < n; i++){
     scanf("%d", &arr[i]);
   }
   
-  printf("Largest number is %d", maximum(arr));
+  printf("Largest number is %d", maximum(arr, n));
   return 0;
 }
