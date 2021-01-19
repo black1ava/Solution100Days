e077bf35bcf50784c420da734dc27774f9e46272
black1ava <73036266+black1ava@users.noreply.github.com>
2021-01-19 23:23:57 +0700

    Update

--- C/OS/Find Max/solution2.c
+++ C/OS/Find Max/solution2.c
@@ -1,22 +1,23 @@
 #include<stdio.h> //time complexity O(1)
 #include<stdlib.h>
 
-#define L 6
-
 int cmp(const void *a, const void *b){
   return *(int *)b - *(int *)a;
 }
 
 int main(){
-  int arr[L];
+  int n;
+  printf("Elements in an array: ");
+  scanf("%d", &n);
+  int arr[n];
   
-  printf("Input 6 integers in one line seperate by space : ");
+  printf("Input %d integers in one line seperate by space : ", n);
   
-  for(int i = 0; i < L; i++){
+  for(int i = 0; i < n; i++){
     scanf("%d", &arr[i]);
   }
   
-  qsort(arr, L, sizeof(int), cmp);
+  qsort(arr, n, sizeof(int), cmp);
   
   printf("Largest number is %d", *arr);
   
